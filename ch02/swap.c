699096bc4589db08913f868e5c6c0e56828da971
Ignacio Oyarzabal <ignaoya93@gmail.com>
2022-05-17 12:10:04 -0300

    New version of swap using void pointers and memcpy

--- ch02/swap.c
+++ ch02/swap.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void swap1(int x, int y); // incorrect swap function
 void swap2(int *x, int *y); // correct swap function
+int swap3(void *x, void *y, int size); // using void pointers
 
 int main(void)
 {
 	int a = 10;
 	int b = 20;
 
-	swap2(&a, &b);
+	swap3(&a, &b, sizeof(int));
 	printf("a: %i - b: %i\n", a, b);
 
 	return 0;
@@ -24,4 +27,17 @@ void swap2(int *x, int *y)
 {
 	int tmp;
 	tmp = *x; *x = *y; *y = tmp;
+}
+
+int swap3(void *x, void *y, int size)
+{
+	void *tmp;
+
+	if ((tmp = malloc(size)) == NULL)
+		return -1;
+
+	memcpy(tmp, x, size); memcpy(x, y, size); memcpy(y, tmp, size);
+	free(tmp);
+
+	return 0;
 }
